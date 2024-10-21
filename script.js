3a4d75fd4da1be0cdadcd6e1b661c95813baeef3
Jeremy <175655267+jncutter@users.noreply.github.com>
2024-10-21 13:19:40 -0500

    Fix reset button

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ const grid = document.querySelector("#drawspace");
 function setGrid() {
     for (let i=0; i < gridSize**2; i++) {
         const box = document.createElement("div");
+        box.class = "box";
         dimension = 500 / gridSize;
         box.style.width = dimension + "px";
         box.style.height = dimension + "px";
         grid.appendChild(box);
+        console.log(i);
 
         box.onmouseover = () => {
             box.style.backgroundColor = "black";
         }
+
+        document.getElementById("reset").addEventListener("click", () => {
+            box.style.backgroundColor = "lightgray";
+        });
     }
 }
-
-
 
 setGrid();
